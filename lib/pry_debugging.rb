85575befb393b3e14ad459dba86fc1742a41608a
Jonathan Gonzalez <Jonathan.gonzalez@powerhrg.com>
2021-08-04 16:20:30 -0400

    adding updated pry_debuging file

--- lib/pry_debugging.rb
+++ lib/pry_debugging.rb
@@ -1,4 +1,9 @@
+#require 'pry'
+
 def plus_two(num)
 	num + 2
-	num
+	#binding.pry
 end
+
+# this issue with this lab was that on line 5, there was an extra "num", that had no real use. 
+# line 4 didnt have to be changed for this lab to work. 
